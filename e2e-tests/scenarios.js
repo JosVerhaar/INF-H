d3f4319ad505fa054dd058ad75613455bb746ead
Imhotep <Imhotep@jos-2.local>
2015-10-04 22:40:22 +0200

    [+] E2E testing

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,41 +2,37 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('my app:', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view1');
+    it('should automatically redirect to / when location hash/fragment is empty', function() {
+        browser.get('index.html');
+        expect(browser.getLocationAbsUrl()).toMatch("/");
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    describe('home.html', function() {
+
+        beforeEach(function() {
+            browser.get('#/');
+        });
+
+        it('should render home.html when user navigates to /home.html', function() {
+            expect(element.all(by.css('[ng-view] h4')).first().getText()).
+                toMatch(/Upcoming movies/);
+        });
+
+        it('should find popular movies', function() {
+            expect(element.all(by.css('[ng-view] h4')).last().getText()).
+                toMatch(/Popular movies/);
+        });
+
+        it('should navigate to /list/upcoming and use the filter', function() {
+            element(by.id('view-upcoming')).click();
+
+            element(by.css('.searchQuery')).sendKeys('US');
+        });
+
     });
 
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
