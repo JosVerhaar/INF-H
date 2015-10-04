a374b014a4f3c0248cb71f888f7a8e8b6a5e504e
Imhotep <Imhotep@jos-2.local>
2015-10-04 22:40:38 +0200

    [+] Unit Testing

--- app/home/home_test.js
+++ app/home/home_test.js
@@ -1,26 +1,140 @@
-beforeEach(module('myApp'));
+'use strict';
 
-describe('HomeCtrl', function() {
+describe('MyApp controllers: ', function() {
 
-    var scope, $httpBackend, ctrl;
 
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-        $httpBackend = _$httpBackend_;
-        $httpBackend.expectGET('http://api.themoviedb.org/3/movie/popular?api_key=ba9b7992000821605fa725399d300a35').respond(
-        {"page":1,"results":[{"id":76341,"release_date":"2015-05-15","popularity":39.92474,"title":"Mad Max: Fury Road"}]});
+    beforeEach(module('myApp.home'));
+    beforeEach(module('ConfigurationService'));
+    beforeEach(module('MovieService'));
 
-        scope = $rootScope.$new();
-        ctrl = $controller('HomeCtrl', {$scope : scope});
+    describe('UpcomingMovieCtrl', function() {
 
-    }));
+        var scope, $httpBackend, ctrl;
 
-    it('should load popular movies', function() {
-        expect(scope.popular).toBeUndefined();
-        $httpBackend.flush();
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('http://api.themoviedb.org/3/movie/upcoming?api_key=ba9b7992000821605fa725399d300a35&language=en&include_image_language=en&page=1').respond(
+                {"page":1,"results":[{"id":76341,"release_date":"2015-05-15","popularity":39.92474,"title":"Mad Max: Fury Road"}]});
 
-        expect(scope.popular).toEqual(
-            {"page":1,"results":[{"id":76341,"release_date":"2015-05-15","popularity":39.92474,"title":"Mad Max: Fury Road"}]}
-        );
+            scope = $rootScope.$new();
+            ctrl = $controller('upcomingMovieCtrl', {$scope : scope});
+
+        }));
+
+        it('results should have an id', function() {
+            expect(scope.upcoming).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.upcoming.results[0].id).toEqual(76341);
+
+        });
+
+        it('results should have a release_date', function() {
+            expect(scope.upcoming).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.upcoming.results[0].release_date).toEqual("2015-05-15");
+        });
+
+        it('results should have a popularity', function() {
+            expect(scope.upcoming).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.upcoming.results[0].popularity).toEqual(39.92474);
+        });
+
+        it('results should have a title', function() {
+            expect(scope.upcoming).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.upcoming.results[0].title).toEqual("Mad Max: Fury Road");
+        });
+    });
+
+    describe('topRatedMovieCtrl', function() {
+
+        var scope, $httpBackend, ctrl;
+
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('http://api.themoviedb.org/3/movie/top_rated?api_key=ba9b7992000821605fa725399d300a35&language=en&include_image_language=en').respond(
+                {"page":1,"results":[{"id":76341,"release_date":"2015-05-15","popularity":39.92474,"title":"Mad Max: Fury Road"}]});
+
+            scope = $rootScope.$new();
+            ctrl = $controller('topRatedMovieCtrl', {$scope : scope});
+
+        }));
+
+        it('results should have an id', function() {
+            expect(scope.topRated).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.topRated.results[0].id).toEqual(76341);
+        });
+
+        it('results should have a release_date', function() {
+            expect(scope.topRated).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.topRated.results[0].release_date).toEqual("2015-05-15");
+        });
+
+        it('results should have a popularity', function() {
+            expect(scope.topRated).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.topRated.results[0].popularity).toEqual(39.92474);
+        });
+
+        it('results should have a title', function() {
+            expect(scope.topRated).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.topRated.results[0].title).toEqual("Mad Max: Fury Road");
+        });
+    });
+
+    describe('popularMovieCtrl', function() {
+
+        var scope, $httpBackend, ctrl;
+
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('http://api.themoviedb.org/3/movie/popular?api_key=ba9b7992000821605fa725399d300a35&language=en&include_image_language=en').respond(
+                {"page":1,"results":[{"id":76341,"release_date":"2015-05-15","popularity":39.92474,"title":"Mad Max: Fury Road"}]});
+
+            scope = $rootScope.$new();
+            ctrl = $controller('popularMovieCtrl', {$scope : scope});
+
+        }));
+
+        it('results should have an id', function() {
+            expect(scope.popular).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.popular.results[0].id).toEqual(76341);
+        });
+
+        it('results should have a release_date', function() {
+            expect(scope.popular).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.popular.results[0].release_date).toEqual("2015-05-15");
+        });
+
+        it('results should have a popularity', function() {
+            expect(scope.popular).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.popular.results[0].popularity).toEqual(39.92474);
+        });
+
+        it('results should have a title', function() {
+            expect(scope.popular).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.popular.results[0].title).toEqual("Mad Max: Fury Road");
+        });
     });
 
 });
--- karma.conf.js
+++ karma.conf.js
@@ -1,35 +1,37 @@
 module.exports = function(config){
-  config.set({
+    config.set({
 
-    basePath : './',
+        basePath : './',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-loader/angular-loader.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/components/**/*.js',
-      'app/**/*.js'
-    ],
+        files : [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+//            'app/bower_components/angular-loader/angular-loader.js',
+//            'app/bower_components/angular-resource/angular-resource.js'
+            'app/home/home.js',
+            'app/services/services.js',
+            'app/components/**/*.js',
+            'app/**/*_test.js'
+        ],
 
-    autoWatch : true,
+        autoWatch : true,
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+        browsers : ['Chrome'],
 
-    plugins : [
-        'karma-chrome-launcher',
-        'karma-firefox-launcher',
-        'karma-jasmine',
-        'karma-junit-reporter'
-    ],
+        plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+        ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+        junitReporter : {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
 
-  });
+    });
 };
